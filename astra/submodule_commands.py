06fcffe5766068065774971fc174164519d3ca0a
John <main@pop-os.localdomain>
2020-03-31 20:24:42 -0500

    Updated old_unarchive again

---
+++ astra/submodule_commands.py
@@ -0,0 +1,93 @@
+from core import *
+import re
+
+def create_submodule(args, message):
+    try:
+        # names being lowercase should help with manipulating submodules later
+        name = args[0].lower()
+        guild = message.guild
+
+        category = await guild.create_category_channel(name)
+        await guild.create_text_channel(f"{name}-general", category=category)
+        await guild.create_text_channel(f"{name}-resources", category=category)
+        await guild.create_voice_channel(f"{name.replace(' ', '-')}-voice", category=category)
+        # For some reason, voice channels do not have the same naming restriction as text channels,
+        # which is the reason for the string manipulation in that last command.
+
+        # Make sure the new category is above the archive category
+        await category.edit(position=category.position - 1)
+    except KeyError:
+        log("[Bot] Not enough arguments for module create command!")
+        # TODO: error relaying to users
+
+    except discord.Forbidden:
+        log("[Bot] Insufficient permissions to create submodule channels.")
+        # TODO: error relaying to users
+
+        return 1
+
+def archive_submodule(args, message):
+    try:
+        name = args[0].lower()
+        guild = message.guild
+
+        category = None
+        for c in guild.categories:
+            if c.name == name:
+                category = c
+                break
+        if category is None:
+            log("[Bot] No submodule with that name found.")
+        else:
+            archive = None
+            for c in guild.categories:
+                if c.name == 'archive':
+                    archive = c
+                    break
+            if archive is None:
+                archive = await guild.create_category_channel('archive')
+
+            for channel in category.channels:
+                if channel.type != discord.ChannelType.voice:
+                    await channel.edit(category=archive)
+                else:
+                    await channel.delete()
+            await category.delete()
+    except KeyError:
+        log("[Bot] Insufficient arguments to archive submodule channels.")
+        # TODO: error relaying to users    
+
+def unarchive_submodule(args, message):
+    try:
+        name = args[0].lower()
+        guild = message.guild
+
+        category = None
+        for c in guild.categories:
+            if c.name == name:
+                category = c
+                break
+
+            archive = None
+            for c in guild.categories:
+                if c.name == 'archive':
+                    archive = c
+                    break
+            if archive is None:
+                log("[Bot] Nothing is archived.")
+
+        category = await guild.create_category_channel(name)
+
+        # Make sure the new category is above the archive category
+        await category.edit(position=category.position - 1)
+
+        # Turn the name into regex to find all channels of that type
+        replaced_name = name.lower().replace(' ', '')
+        for channel in archive.channels:
+            if replaced_name in channel.name.replace('-',''):
+                await channel.edit(category=category)
+        await guild.create_voice_channel(f"{name.replace(' ', '-')}-voice", category=category)
+
+    except KeyError:
+        log("[Bot] Insufficient arguments to archive submodule channels.")
+        # TODO: error relaying to users
